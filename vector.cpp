a2409328ce3c5b864382923b8c6249a8d37bd860
LOKESH PANDEY <73874665+lokeshs333@users.noreply.github.com>
2022-07-22 13:08:04 +0530

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -5,10 +5,16 @@ int main()
 {
     vector<pair<int, int>> arr;
     arr.push_back({1, 45});
+    arr.push_back({2, 5});
+    arr.push_back({121, 4});
     arr.push_back({12, 555});
-    for (int i = 0; i < arr.size(); i++)
+    arr.insert(arr.begin(),{12,95});
+    arr.insert(arr.begin()+1,2,{1,1});
+    for (int i = 0; i < arr.size();i++)
     {
         cout << arr[i].first << " " << arr[i].second << endl;
     }
+    
+
     return 0;
 }
